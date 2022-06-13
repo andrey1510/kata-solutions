795d1679c7c3db65a8554d63f252d29fd6c6c46a
andrey1510 <andrey-1510@outlook.com>
2022-06-13 20:12:03 +0300

    Update

--- codewars/src/level8/VolumeOfCuboid.java
+++ codewars/src/level8/VolumeOfCuboid.java
@@ -9,3 +9,25 @@ public class VolumeOfCuboid {
     System.out.println(getVolumeOfCuboid(2,3,4));
   }
 }
+
+
+//////////////////////////////////////////////
+
+class VolumeOfCuboidL {
+  public static void getVolumeOfCuboid(Calculator c) {
+    double length = 3.4;
+    double width = 2.8;
+    double height = 4.2;
+    double result = c.calculate(length,width,height);
+    System.out.println(result);
+  }
+
+  public static void main(String[] args) {
+    getVolumeOfCuboid((length,width,height) -> length*width*height);
+  }
+}
+
+@FunctionalInterface
+interface Calculator{
+  double calculate(double length, double width, double height);
+}
