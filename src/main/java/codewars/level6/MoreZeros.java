6375cbd6df2fd8304c94f2c729ad16dad86b8050
andrey1510 <andrey-1510@outlook.com>
2023-02-16 03:21:03 +0300

    new solutions added

--- src/main/java/codewars/level6/MoreZeros.java
+++ src/main/java/codewars/level6/MoreZeros.java
@@ -27,15 +27,3 @@ public class MoreZeros {
     }
 }
 
-class MoreZerosTests {
-    @Test
-    void basicTests() {
-        assertArrayEquals(new char[] {'a','b','d','h','p','A','B','C','D','E','F','H','I','J','L','P','Q','R','T','X','0'}, MoreZeros.moreZeros("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_"));
-        assertArrayEquals(new char[] {'D', 'I', 'E', 'T'}, MoreZeros.moreZeros("DIGEST"));
-        assertArrayEquals(new char[] {'a','b','d'}, MoreZeros.moreZeros("abcde"));
-        assertArrayEquals(new char[] {'h', 'b', 'p', 'a', 'd'}, MoreZeros.moreZeros("thequickbrownfoxjumpsoverthelazydog"));
-        assertArrayEquals(new char[] {'T','H','E','Q','I','C','B','R','F','X','J','P','L', 'A','D'}, MoreZeros.moreZeros("THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"));
-
-    }
-}
-
